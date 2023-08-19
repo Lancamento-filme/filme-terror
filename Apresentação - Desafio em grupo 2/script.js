35294b07fca90cf94367a55953004b4531eb6867
Samuel_Ramos <progsamu@gmail.com>
2023-08-19 11:19:45 -0300

    correção código

--- Apresentação - Desafio em grupo 2/script.js
+++ Apresentação - Desafio em grupo 2/script.js
@@ -1,5 +1,4 @@
-
-window.sr = ScrollReveal({reset: true});
+window.sr = ScrollReveal({ reset: true });
 
 // Define the blinking background animation
 function blinkBackground() {
@@ -7,16 +6,15 @@ function blinkBackground() {
 }
 
 // Trigger blinking background animation when elements are revealed
-sr.reveal('#home', {duration: 1000, afterReveal: blinkBackground});
-sr.reveal('#descricao', {duration: 1000});
-sr.reveal('#elenco', {duration: 1000});
-sr.reveal('#assista', {duration: 3500});
+sr.reveal("#home", { duration: 1000, afterReveal: blinkBackground });
+sr.reveal("#descricao", { duration: 1000 });
+sr.reveal("#elenco", { duration: 1000 });
+sr.reveal("#assista", { duration: 3500 });
 // sr.reveal('#teaser', {duration: 2000});
 // sr.reveal('#assisectionComplementarsta', {duration: 2000});
-sr.reveal('#curiosidade1', {duration: 2000});
-sr.reveal('#curiosidade2', {duration: 2500});
-sr.reveal('#curiosidade3', {duration: 3000});
+sr.reveal("#curiosidade1", { duration: 2000 });
+sr.reveal("#curiosidade2", { duration: 2500 });
+sr.reveal("#curiosidade3", { duration: 3000 });
 
 // Set an interval to keep the blinking background animation going
-setInterval(blinkBackground, 5000); // Blink every 5 seconds
-
+setInterval(blinkBackground, 5000);
