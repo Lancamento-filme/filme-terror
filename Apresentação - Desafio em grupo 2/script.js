d7eb6d9df05853a0a47113cb5a68a6d5b4139fb8
Samuel_Ramos <progsamu@gmail.com>
2023-08-20 16:56:49 -0300

    suavização da transição

--- Apresentação - Desafio em grupo 2/script.js
+++ Apresentação - Desafio em grupo 2/script.js
@@ -18,3 +18,41 @@ sr.reveal("#curiosidade3", { duration: 3000 });
 
 // Set an interval to keep the blinking background animation going
 setInterval(blinkBackground, 5000);
+
+const bodyElement = document.querySelector("body");
+
+const imagesToPreload = [
+  "./background/Background.png",
+  "./background/10.png",
+  "./background/20.png",
+  "./background/30.png",
+  "./background/40.png",
+  "./background/50.png",
+  "./background/60.png",
+  "./background/70.png",
+  "./background/80.png",
+  "./background/90.png",
+  "./background/100.png"
+];
+
+function preloadImages(images, callback) {
+  let loadedImages = 0;
+
+  function imageLoaded() {
+    loadedImages++;
+    if (loadedImages === images.length) {
+      callback();
+    }
+  }
+
+  images.forEach(imageSrc => {
+    const img = new Image();
+    img.src = imageSrc;
+    img.onload = imageLoaded;
+  });
+}
+
+preloadImages(imagesToPreload, () => {
+  bodyElement.classList.remove("preload");
+});
+
